145f8b43a01e52dc436cc44bf4a2bdc91e7a73c3
Kuheng <leems082042@naver.com>
2024-06-11 17:06:11 +0900

    Apply i18n library , Add toggleLanguage function Edit contents on label

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const onClick_toggleLocales = useCallback(() => {
+    (i18n.language === 'ko-KR')? i18n.changeLanguage('en-US'):i18n.changeLanguage('ko-KR');
+  },[i18n]
+  );
+
   return (
     <div className="flex w-screen h-screen min-w-75vw">
       <div className="w-72 h-screen min-w-72 bg-gray-1">
@@ -27,10 +36,13 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="w-main h-calc(-20) min-w-75vw mt-5 ml-5 bg-white">
-        <div className="flex items-center w-620 h-12 ml-auto">
-          <p className="text-center w-24 h-auto text-xl">API키</p>
-          <input type="text" className="w-440 h-11 pl-1 border-b-2 border-b-black text-lg outline-none" placeholder="* API 키 입력 필수" />
+      <div className="w-main h-calc(-20) min-w-75vw mt-1 ml-5 bg-white">
+        <div className="flex items-center w-640 h-20 ml-auto">
+          <div className="flex items-center w-560 h-12">
+            <p className="text-center w-24 h-auto text-xl">{t("main:apiKey")}</p>
+            <input type="text" className="w-440 h-11 pl-1 border-b-2 border-b-black text-lg outline-none" placeholder={"* " + t("main:apiKeyInputPlaceholder")} />
+          </div>
+          <p className="w-auto h-auto text-xl cursor-pointer select-none" onClick={()=>onClick_toggleLocales()}>{t("main:languageChangeLabel")}</p>
         </div>
         <div className="text-centerw-full h-700">
           <img src={logo} className="w-11/12 h-full" alt="" />
@@ -51,36 +63,42 @@ function App() {
         </div>
         <div className="flex w-full h-auto min-w-1024 mt-3">
           <div className="w-calc(-20) h-full">
-            <p className="w-auto h-auto text-lg">제시어 프롬프트</p>
-            <textarea className="w-calc(-20) h-54px border-2 border-gray-1 rounded-md p-2 text-lg outline-none resize-none" placeholder="제시어의 구분은 ,로 구분합니다." />
-            <p className="w-auto h-auto text-lg">제외 항목 프롬프트</p>
-            <textarea className="w-calc(-20) h-54px border-2 border-gray-1 rounded-md p-2 text-lg outline-none resize-none" placeholder="제시어의 구분은 ,로 구분합니다." />
+            <p className="w-auto h-auto text-lg">* {t("main:imagePrompt")}
+              <span className="w-auto h-auto ml-10 text-sm text-gray-2">{t("main:promptAlertMessage")}</span></p>
+            <textarea className="w-calc(-20) h-54px border-2 border-gray-1 rounded-md p-2 text-lg outline-none resize-none" />
+            <p className="w-auto h-auto text-lg">{t("main:negativePrompt")}</p>
+            <textarea className="w-calc(-20) h-54px border-2 border-gray-1 rounded-md p-2 text-lg outline-none resize-none" />
           </div>
           <div className="text-center w-300 h-auto mx-3">
             <div className="w-full h-14 text-start">
-              <p className="w-auto h-auto text-lg">시드</p>
-              <input type="range" className="w-full h-auto" />
+              <p className="w-auto h-auto text-lg">{t("main:seedLabel")}</p>
+              <input type="range" value="" onChange={()=>{}} className="w-full h-auto" />
             </div>
             <div className="flex w-full h-auto">
               <div className="text-start w-49% h-auto mr-auto">
-                <p className="w-full h-auto text-lg">확장자</p>
+                <p className="w-full h-auto text-lg">{t("main:formatLabel")}</p>
                 <select className="w-full h-9 text-lg outline-none">
-                  <option value="">test</option>
-                  <option value="">test</option>
-                  <option value="">test</option>
+                  <option value="none">none</option>
+                  <option value="png" selected>png</option>
+                  <option value="jpg">jpg</option>
                 </select>
               </div>
               <div className="text-start w-49% h-auto">
-                <p className="w-full h-auto text-lg">사이즈</p>
+                <p className="w-full h-auto text-lg">{t("main:aspectLabel")}</p>
                 <select className="w-full h-9 text-lg outline-none">
-                  <option value="">test</option>
-                  <option value="">test</option>
-                  <option value="">test</option>
+                  <option value="none">none</option>
+                  <option value="1:1" selected>1:1</option>
+                  <option value="4:3">4:3</option>
+                  <option value="16:9">16:9</option>
+                  <option value="21:9">21:9</option>
+                  <option value="3:4">3:4</option>
+                  <option value="9:16">9:16</option>
+                  <option value="9:21">9:21</option>
                 </select>
               </div>
             </div>
           </div>
-          <button className="w-300 h-auto border-2 border-gray-1 rounded-sm bg-primary-1 cursor-p">실행</button>
+          <button className="w-300 h-auto border-2 border-gray-1 rounded-sm bg-primary-1 text-xl cursor-p">{t("main:generateButton")}</button>
         </div>
       </div>
     </div>
