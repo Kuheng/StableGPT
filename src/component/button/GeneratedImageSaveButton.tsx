8f86377e8239ac15d139950d7e2492acd1554806
Kuheng <leems082042@naver.com>
2024-09-09 17:37:49 +0900

    Add function to Save generated image

--- src/component/button/GeneratedImageSaveButton.tsx
+++ src/component/button/GeneratedImageSaveButton.tsx
@@ -1,22 +1,21 @@
 import { useTranslation } from "react-i18next";
 import { useRecoilValue } from "recoil";
 
+import { stableDiffusionParameterSelector } from "../../states/stableDiffusionParameterState";
 import { generatedImageSelector } from "../../states/generatedImageState";
 
 import logo from "../../logo.svg"
 
 function GeneratedImageSaveButton () {
+  const stableDiffusionParameter = useRecoilValue(stableDiffusionParameterSelector);
   const generatedImage = useRecoilValue(generatedImageSelector).base64;
+
   const { t } = useTranslation();
-
-  function onClickSaveImage () {
-
-  }
 
   return (
     <>{generatedImage !== logo?<button className="w-full h-10 mb-2 border-1 rounded-5px border-blue-1 bg-blue-1 text-white
-      active:border-blue-2  active:bg-blue-2 text-lg font-semibold cursor-pointer"
-    onClick={onClickSaveImage}>{t("main:saveImage")}</button>
+      active:border-blue-2  active:bg-blue-2 text-lg font-semibold cursor-pointer">
+        <a href={generatedImage} download={t("main:saveImageName")+stableDiffusionParameter.format}>{t("main:saveImage")}</a></button>
     :<button className="w-full h-10 mb-2 border-1 border-gray-5 rounded-5px bg-gray-6 text-gray-3 text-lg font-semibold cursor-not-allowed">
     {t("main:saveImage")}</button>}</>
   );
